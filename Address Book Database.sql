548b865cf7cf3e1714327defe67a249eb5f1a28e
vpradhap <pradhapvenkat@gmail.com>
2022-06-19 22:28:53 +0530

    Added : Created table in the database

--- Address Book Database.sql
+++ Address Book Database.sql
@@ -1,3 +1,6 @@
 --UC1 Create database
 create database AddressBookService
 use AddressBookService
+--UC2 Create table
+create table Addressbook(firstname varchar(100),lastname varchar(100),address varchar(200),city varchar(100),state varchar(100),zip int,phone int,email varchar(100))
+select * from Addressbook
